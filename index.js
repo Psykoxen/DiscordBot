2494f76f08b8cc31e8a2cdfa5419059a8862460c
Psykoxen <58462517+Psykoxen@users.noreply.github.com>
2021-02-14 12:41:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const {CommandoClient} = require('discord.js-commando');
+const { type } = require('os');
+const path = require('path');
+
+const client = new CommandoClient({
+	commandPrefix: '*',
+	owner: '357532006193692672',
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerGroup('gif','Gif')
+	.registerGroup('music','Music')
+	.registerGroup('brawlstar','BrawlStar')
+	.registerCommandsIn(path.join(__dirname, 'commands'))
+
+client.server = {
+	queue: [],
+	repeat: false,
+	currentVideo: {title: "", url: ""},
+	dispatcher: null,
+	connection: null
+}
+
+client.once('ready', () => {
+	console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
+	client.user.setStatus('dnd');
+	client.user.setActivity("comment fonctionner", {type: 'WATCHING'});
+})
+
+client.on('error' , (error) => console.error(error));
+
+client.login(process.env.TOKEN);
