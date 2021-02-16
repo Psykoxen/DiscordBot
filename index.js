c4a3c119c03b0c53a094a8b54b5d4c4bda41ac54
Psykoxen <58462517+Psykoxen@users.noreply.github.com>
2021-02-16 14:23:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { type } = require('os');
 const path = require('path');
 
 const client = new CommandoClient({
-	commandPrefix: '$',
+	commandPrefix: '*',
 	owner: '357532006193692672',
 });
 
@@ -18,22 +18,19 @@ client.registry
 	.registerCommandsIn(path.join(__dirname, 'commands'))
 
 client.server = {
-	queue: {},
+	queue: [],
 	repeat: false,
-	currentVideo: {},
-	dispatcher: {},
-	connection: {},
-
+	currentVideo: {title: "", url: ""},
+	dispatcher: null,
+	connection: null
 }
 
 client.once('ready', () => {
 	console.log(`Connecté en tant que ${client.user.tag} - (${client.user.id})`);
 	client.user.setStatus('dnd');
-	client.user.setActivity("comment s'améliorer", {type: 'WATCHING'});
+	client.user.setActivity("comment fonctionner", {type: 'WATCHING'});
 })
 
 client.on('error' , (error) => console.error(error));
 
-client.login('ODA3MjUwOTQ5MjY1MjkzMzIz.YB1RCA.3-mJm80xv0CqeMzfosdtU0V2CK0');
-// process.env.TOKEN
-// 'ODA2NTU4MjAwNTE3NzU0ODkx.YBrL3A.8bntsXE8vxHqrR1qFOG7c9MWwUI'
+client.login(process.env.TOKEN);
