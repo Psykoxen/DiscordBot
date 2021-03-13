6b2130bbb7ceb3fec3f22c33bedf12df42f46376
Psykoxen <58462517+Psykoxen@users.noreply.github.com>
2021-03-13 17:51:56 +0100

    Add files via upload

---
+++ leave.js
@@ -0,0 +1,37 @@
+const { Command, CommandoMessage } = require("discord.js-commando");
+const { UserNotInVoiceChannel,BotNotInVoiceChannel } = require('../../error.json')
+
+module.exports = class LeaveCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'leave',
+            aliases: ['l'],
+            group: 'music',
+            memberName: 'leave',
+            description: 'Déconnecte le bot du salon vocal'
+        });
+    }
+
+    /**
+     * 
+     * @param {CommandoMessage} message 
+     */
+    async run(message) {
+        const voiceChannel = message.member.voice.channel;
+        const server = message.client.server;
+        
+        if (!voiceChannel) {
+            return message.say(UserNotInVoiceChannel);
+        }
+        if  (!message.client.voice.connections.first()) {
+            return message.say(BotNotInVoiceChannel);
+        }
+        if (server.currentVideo[voiceChannel.id].url = '' ) {
+            server.connection[voiceChannel.id].dispatcher.end();
+            server.currentVideo[voiceChannel.id] = {title: '', url: ''};
+        }
+        server.queue[voiceChannel.id] = [];  
+        server.repeat = false;
+        await voiceChannel.leave();
+    }
+}
---
+++ queue.js
@@ -0,0 +1,83 @@
+const { MessageEmbed } = require("discord.js");
+const { Command, CommandoMessage } = require("discord.js-commando");
+const { BotNotInVoiceChannel, EmptyQueue, OutQueue } = require('../../error.json')
+
+module.exports = class QueueCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'queue',
+            aliases: ['q'],
+            group: 'music',
+            memberName: 'queue',
+            description: "Affiche la file d'attente",
+            args: [
+                {
+                key: 'page',
+                prompt: "Quel page veux-tu afficher ?",
+                default: 1,
+                type: 'integer'
+                }
+            ]
+        });
+    }
+
+    /**
+     * 
+     * @param {CommandoMessage} message
+     * @param {Number} page
+     */
+    async run(message, { page }) {
+        const voiceChannel = message.member.voice.channel;
+        const server = message.client.server;
+        
+        if  (!message.client.voice.connections.first()) {
+            return message.say(BotNotInVoiceChannel);
+        }
+        console.log(server.queue[voiceChannel.id])
+        if (!server.queue[voiceChannel.id][0]){
+            return message.say(EmptyQueue);
+        }
+        const numberItems = 10;
+        const startingItem = (page - 1) * numberItems;
+        const queueLength = server.queue[voiceChannel.id].length;
+
+        var itemPage = startingItem + numberItems;
+        var totalPages = 1;
+
+        if (server.repeat[voiceChannel.id] == true)  {
+            var repeat = ':white_check_mark:';
+        } else {
+            var repeat = ':x:';
+        }  
+        
+        var embed = new MessageEmbed()
+            .setTitle("File d'attente                 Repeat " + repeat)
+            .setColor("#8229de")
+            .addField('En train de jouer : ', server.currentVideo[voiceChannel.id].title);
+        
+            
+        if (queueLength > 0) {
+            var value = "";
+
+            if (queueLength > numberItems) {
+                totalPages = Math.ceil(queueLength / numberItems);
+            }
+
+            if (page < 0 || (page) > totalPages ) {
+                return message.say(OutQueue);
+            }
+
+            if ((queueLength - startingItem) < numberItems) {
+                itemPage = (queueLength - startingItem) + startingItem;
+            }
+
+            for (let i = startingItem; i < itemPage; i++) {
+                const video = server.queue[voiceChannel.id][i];
+                value +=  "`" + (i+1) + ". `" + video.title + "\n";
+            }
+            embed.addField("A venir :", value);
+        }
+        embed.setFooter(`Page ${page}/${totalPages}`);
+        return message.say(embed)
+    }
+}
